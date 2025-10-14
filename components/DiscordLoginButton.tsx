4cb648c7599554ce82d07436fa913b4443658615
SoftMissT <nelsondesigner18@gmail.com>
2025-10-14 17:44:02 -0300

    feat: Improve Discord login button styling and tooltip
    Update global CSS to include Montserrat font and refine tooltip styling for the Discord login button. The button now has a more modern look and a smoother hover effect with a tooltip.

--- components/DiscordLoginButton.tsx
+++ components/DiscordLoginButton.tsx
@@ -8,10 +8,8 @@ interface DiscordLoginButtonProps {
 export const DiscordLoginButton: React.FC<DiscordLoginButtonProps> = ({ onClick }) => {
   return (
     <div className="tooltip-container" onClick={onClick}>
-      <div className="borde-back">
-        <div className="icon">
-          <DiscordIcon className="w-8 h-8" />
-        </div>
+      <div className="icon">
+        <DiscordIcon className="w-8 h-8" />
       </div>
       <div className="tooltip">
         Logar com o Discord
